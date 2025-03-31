135a9b9c3dc1241f7d6dcd59d34b2249de1cffc5
womimc <138317871+womimc@users.noreply.github.com>
2025-03-31 17:06:58 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -64,4 +64,4 @@ clear
 
 $ROOTFS_DIR/usr/local/bin/proot \
   --rootfs="${ROOTFS_DIR}" \
-  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
+  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit su
